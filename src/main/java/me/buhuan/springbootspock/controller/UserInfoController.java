21571e86aae2cbe56a998fee765298f69f144a67
黄步欢 <buhuanhuang@gmail.com>
2017-12-10 17:51:17 +0800

    添加控制层根据用户id查询用户方法和保存用户信息方法

--- src/main/java/me/buhuan/springbootspock/controller/UserInfoController.java
+++ src/main/java/me/buhuan/springbootspock/controller/UserInfoController.java
@@ -1,9 +1,9 @@
 package me.buhuan.springbootspock.controller;
 
+import me.buhuan.springbootspock.bean.UserInfo;
 import me.buhuan.springbootspock.service.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author hbh
@@ -16,5 +16,14 @@ public class UserInfoController {
 	
 	@Autowired
 	private UserInfoService userInfoService;
-
+	
+	@GetMapping("/{userId}")
+	public UserInfo findbyUserId(@PathVariable("userId") Long userId) {
+		return userInfoService.findByUserId(userId);
+	}
+	
+	@PostMapping
+	public UserInfo save(UserInfo userInfo) {
+		return userInfoService.save(userInfo);
+	}
 }
