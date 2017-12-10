24e115efd5efe9e118bbdb7ac19fe9ad55af132c
黄步欢 <buhuanhuang@gmail.com>
2017-12-10 16:53:56 +0800

    添加测试sql语句

---
+++ src/test/groovy/me/buhuan/controller/user/UserControllerSpecification.java
@@ -0,0 +1,11 @@
+package me.buhuan.controller.user;
+
+import me.buhuan.controller.BaseControllerSpecification;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/12/10
+ */
+public class UserControllerSpecification extends BaseControllerSpecification {
+}
