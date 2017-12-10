3b116149165b535d347b84ce7a7ef8ddb16f4c02
黄步欢 <buhuanhuang@gmail.com>
2017-12-10 17:39:51 +0800

    添加用户信息dao层接口

--- src/test/groovy/me/buhuan/controller/user/UserControllerSpec.groovy
+++ src/test/groovy/me/buhuan/controller/user/UserControllerSpec.groovy
@@ -2,6 +2,7 @@ package me.buhuan.controller.user;
 
 import me.buhuan.controller.BaseControllerSpec
 import me.buhuan.springbootspock.bean.UserInfo
+import me.buhuan.springbootspock.dao.UserInfoRepository
 import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator
 
@@ -43,7 +44,22 @@ class UserControllerSpec extends BaseControllerSpec {
 		res.name == 'hbh'
 	}
 
-	def "Test REST Save userInfo"() {
-		
+	def "Test REST Save UserInfo"() {
+		when:
+		def result = post("user", userInfo)
+
+		then:
+		result.andExpect(status().isOk())
+
+		and:
+		def res = result2Res(result.andReturn())
+
+		then:
+		res.id == id
+
+		where:
+		userInfo || id
+		new UserInfo(userId: 1003, name: "ls", sex: 1, address: "北京") || 3
+		new UserInfo(userId: 1004, name: "ww", sex: 1, address: "广州") || 4
 	}
 }
