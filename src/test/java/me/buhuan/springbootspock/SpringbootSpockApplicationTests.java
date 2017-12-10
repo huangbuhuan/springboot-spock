c91b9eb6259c83204d9a49cb4f93cc852928cf89
黄步欢 <buhuanhuang@gmail.com>
2017-12-10 15:49:56 +0800

    初始化:tada:

---
+++ src/test/java/me/buhuan/springbootspock/SpringbootSpockApplicationTests.java
@@ -0,0 +1,16 @@
+package me.buhuan.springbootspock;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootSpockApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
