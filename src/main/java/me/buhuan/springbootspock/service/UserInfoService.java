f5aec6a9a998e877cbdff05424766821a3be917d
黄步欢 <buhuanhuang@gmail.com>
2017-12-10 17:48:59 +0800

    添加根据用户id查询用户信息和保存用户信息方法

--- src/main/java/me/buhuan/springbootspock/service/UserInfoService.java
+++ src/main/java/me/buhuan/springbootspock/service/UserInfoService.java
@@ -1,5 +1,6 @@
 package me.buhuan.springbootspock.service;
 
+import me.buhuan.springbootspock.bean.UserInfo;
 import me.buhuan.springbootspock.dao.UserInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,4 +15,12 @@ public class UserInfoService {
 	
 	@Autowired
 	private UserInfoRepository userInfoRepository;
+	
+	public UserInfo save(UserInfo userInfo) {
+		return userInfoRepository.save(userInfo);
+	}
+	
+	public UserInfo findByUserId(Long userId) {
+		return userInfoRepository.findByUserId(userId);
+	}
 }
